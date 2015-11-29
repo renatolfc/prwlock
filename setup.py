d52d4091d982d81f65f4d8b680ce3a3b8d7415e4
Renato Cunha <renato@renatocunha.com>
2015-11-29 17:06:26 -0200

    add a setup file

---
+++ setup.py
@@ -0,0 +1,45 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+import prwlock
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='prwlock',
+    version=prwlock.__version__,
+    description='Native process-shared rwlock support for Python',
+    long_description=long_description,
+    url='https://github.com/trovao/prwlock',
+    author='Renato Cunha',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Operating System',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: POSIX :: BSD :: FreeBSD',
+        'Operating System :: POSIX :: BSD :: OpenBSD',
+        'Operating System :: MacOS :: MacOS X',
+    ],
+    keywords='rwlock posix process-shared process',
+    packages=find_packages(),
+)
