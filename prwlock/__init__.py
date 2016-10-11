22f79754433eef0970cfa8877742fd4493a435a1
Renato L. F. Cunha <renato@renatocunha.com>
2016-10-11 11:10:56 -0300

    prwlock: drop excessive whitespace

--- prwlock/__init__.py
+++ prwlock/__init__.py
@@ -7,7 +7,6 @@ __version__ = '0.3.0'
 # Note: names are added to __all__ depending on what system we're on
 __all__ = []
 
-
 if platform.system() == 'Windows':
     from wrwlock import RWLockWindows as RWLock
 else:
